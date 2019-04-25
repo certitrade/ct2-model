76512971ebf21155390ba48dc58a5d01c4ebb64d
Simon Mika <simon@mika.se>
2019-04-25 18:17:48 +0200

    Removed types available in isoly.

--- source/hal/Collection.ts
+++
@@ -1,6 +0,0 @@
-import { Resource } from "./Resource"
-
-export interface Collection extends Resource {
-	"max_page_size": string,
-	"total_size": string
-}
--- source/hal/Resource.ts
+++
@@ -1,6 +0,0 @@
-import { Links } from "./Links"
-
-export interface Resource {
-	_links?: Links
-	_embedded?: []
-}
